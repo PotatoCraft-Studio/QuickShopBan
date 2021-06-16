aef9e7f07497141152707e62adc985991ca1be31
Ghost_chu <2908803755@qq.com>
2021-06-16 18:43:36 +0800

    Update to latest QuickShop build

--- src/main/java/studio/potatocraft/quickshopban/UnbanCommand.java
+++ src/main/java/studio/potatocraft/quickshopban/UnbanCommand.java
@@ -3,31 +3,26 @@ package studio.potatocraft.quickshopban;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.block.Block;
-import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.util.BlockIterator;
 import org.maxgamer.quickshop.api.QuickShopAPI;
-import org.maxgamer.quickshop.command.CommandProcesser;
+import org.maxgamer.quickshop.command.CommandHandler;
 import org.maxgamer.quickshop.shop.Shop;
 import org.maxgamer.quickshop.util.MsgUtil;
 
 import java.util.List;
 import java.util.Optional;
 
-public class UnbanCommand implements CommandProcesser {
+public class UnbanCommand implements CommandHandler<Player> {
     private final QuickShopBan plugin;
     public UnbanCommand(QuickShopBan plugin){
         this.plugin = plugin;
     }
     @Override
-    public void onCommand(CommandSender commandSender, String s, String[] strings) {
+    public void onCommand(Player commandSender, String s, String[] strings) {
         if(strings.length < 1){
             MsgUtil.sendMessage(commandSender,MsgUtil.getMessage("wrong-args", commandSender));
-            return;
-        }
-        if(!(commandSender instanceof LivingEntity)){
-            commandSender.sendMessage("Only player can execute this command.");
             return;
         }
         OfflinePlayer player = Bukkit.getOfflinePlayer(strings[0]);
@@ -36,7 +31,7 @@ public class UnbanCommand implements CommandProcesser {
             return;
         }
 
-        final BlockIterator bIt = new BlockIterator((LivingEntity) commandSender, 10);
+        final BlockIterator bIt = new BlockIterator(commandSender, 10);
 
         if (!bIt.hasNext()) {
             MsgUtil.sendMessage(commandSender, MsgUtil.getMessage("not-looking-at-shop", commandSender));
